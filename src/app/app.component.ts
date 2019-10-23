8c1ce1f0acaf1a256a471b3232358fd4681507b7
Lucas Acevedo <lucasacevedo91@gmail.com>
2019-10-23 15:38:17 +0100

    App working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'routing-project';
+export class AppComponent implements OnInit {
+  title = 'project with http calls';
+  users: any[] = [];
+
+  constructor(protected _userService: UserService){  }
+
+  ngOnInit(){
+    this._userService.getUsers()
+      .subscribe(
+        (data)=> {
+          this.users = data['results'];
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
 }
